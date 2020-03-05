32421028696211c13b55271ff02be513ac80090f
Melazer <alissonsegecs@hotmail.com>
2020-03-04 23:09:51 -0300

    I have finished the layout

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from './src/pages/Main/index'
+import TabBar from './src/components/TabBar';
 
 const app = () => {
   return(
     <React.Fragment>
       <Main />
+      <TabBar />
     </React.Fragment>
   )
   };
